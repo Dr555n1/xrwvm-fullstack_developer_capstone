69206cdd97b0f2b0f81007f77efd6ad55b7e5663
vladimir <grinfeldsv75@yahoo.com>
2025-04-12 17:41:00 -0400

    module 4 changes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -35,6 +35,7 @@ def get_request(endpoint, **kwargs):
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
+    print('---------->',request_url,'<--------')
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -47,6 +48,7 @@ def analyze_review_sentiments(text):
 # Add code for posting review
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
+    print('---------->',request_url,'<--------')
     try:
         response = requests.post(request_url,json=data_dict)
         print(response.json())
